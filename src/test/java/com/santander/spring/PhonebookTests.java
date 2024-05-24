a91abeeac7ab4c3ed899d60762d343f0068a2543
rolemberg <vinnie.nsts@gmail.com>
2024-05-24 10:29:16 -0300

    testing class Phonebook

--- src/test/java/com/santander/spring/PhonebookTests.java
+++ src/test/java/com/santander/spring/PhonebookTests.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootTest
 public class PhonebookTests {
 
@@ -18,6 +21,28 @@ public class PhonebookTests {
         phonebook.addPhone("Megan Louren", "66448383830");
         assertEquals("11998765533", phonebook.getPhonebook().get("James Wolves"));
         assertEquals("66448383830", phonebook.getPhonebook().get("Megan Louren"));
+    }
+
+    @Test
+    void testingRemovePhoneByName() {
+        phonebook.addPhone("James Wolves", "11998765533");
+        phonebook.addPhone("Megan Louren", "66448383830");
+        assertEquals(2, phonebook.getPhonebook().size());
+        phonebook.removePhone("Megan Louren");
+        assertEquals(1, phonebook.getPhonebook().size());
+        assertEquals("11998765533", phonebook.getPhonebook().get("James Wolves"));
+    }
+
+    @Test
+    void testingFindByName() {
+        phonebook.addPhone("James Wolves", "11998765533");
+        phonebook.addPhone("Megan Louren", "66448383830");
+        Map<String, String> stringMap = new HashMap<>();
+        stringMap.put("James Wolves", "11998765533");
+        assertEquals(stringMap, phonebook.findByName("James Wolves"));
+        stringMap.remove("James Wolves");
+        stringMap.put("Megan Louren", "66448383830");
+        assertEquals(stringMap, phonebook.findByName("Megan Louren"));
     }
 }
 
